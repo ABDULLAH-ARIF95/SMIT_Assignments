2b82fced2d09100225875b6a89a795fa4c7214a6
Abdullah Arif <kb694294@gmail.com>
2024-08-28 14:00:15 +0500

    some changes

---
+++ JS Assignment/Alert/app.js
@@ -0,0 +1,23 @@
+//question 1
+alert("Welcome to my website!");
+
+//question 2
+alert("Error! Please enter a valid password.");
+
+//question 3
+alert("Welcome to JS Land..\nHappy Coding");
+
+//question 4
+alert("Welcome to JS Land");
+alert("Happy Coding!\nPrevent this page from additional dialogs.");
+
+//question 5
+var messageAlert = "Hello.. I can run JS through my web browser's console";
+alert(messageAlert);
+console.log(messageAlert);
+
+//question 6
+//answer is in gmail clone
+
+//question 7
+//answer is in html file
