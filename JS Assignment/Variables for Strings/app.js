f2614822a150d600ce54f2c41d5a6fcd961d11f8
Abdullah Arif <kb694294@gmail.com>
2024-08-28 17:15:54 +0500

    commit

---
+++ JS Assignment/Variables for Strings/app.js
@@ -0,0 +1,35 @@
+//question 1
+var username = "Abdullah-Arif95";
+
+//question 2
+var myName = "Abdullah Arif";
+
+//question 3
+var message = "Hello World";
+alert(message);
+
+//question 4
+var fullName = "Abdullah Arif";
+alert(fullName);
+var age = "17";
+alert(age);
+var courseName = "WMA Development";
+alert(courseName);
+
+//question 5
+var food = "Pizza\nPizz\nPiz\nPi\nP";
+alert(food);
+
+//question 6
+var Email = "kb694294@gmail.com";
+alert("My email address is " + Email);
+
+//question 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+//question 8
+document.write("Yah! I can write html content through Javascript")
+//question 9
+var designedText = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(designedText);
